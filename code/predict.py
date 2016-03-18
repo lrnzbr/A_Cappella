c71b258ce4482c0a95cc7ac9f35a70f206f486b2
Lorenzo Brown <lrnzbr@gmail.com>
2016-03-17 21:07:50 -0700

    Update predict.py

--- code/predict.py
+++ code/predict.py
@@ -1,12 +1,22 @@
 from time import sleep
+import time
+import numpy as np
+import pandas as pd
 
 def predict(fingerprint_queue,prediction_queue, model):
     while True:
         if not fingerprint_queue.empty():
+            print "Predictor Worker waking up...\n"
             fingerprint = fingerprint_queue.get()
-            prediction = model.predict_proba(fingerprint)
+
+            X = fingerprint[0].reshape(1, -1)
+            prediction = model.predict(X)
             print "PREDICTION MADE AT: ", prediction
-            prediction_queue.put(prediction)
+            print "TIME ELAPSED: ", time.time() - fingerprint[1]
+            prediction_queue.put([prediction, fingerprint[1]])
+
+
+
         else:
-            print "Predictor worker waiting....\n"
-            sleep(1)
+            #print "Predictor worker waiting....\n"
+            sleep(.2)
